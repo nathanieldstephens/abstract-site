0b4e68dacd7fe2d8a425810203d27cbaedb67fda
Unknown <nathaniel.drew.stephens@gmail.com>
2018-02-14 13:38:51 -0800

    Initialize GitHub Directory

---
+++ index.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abstract - Float Grid Site</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+<!-- Start Menu -->
+
+<!-- Making Changes -->
+
+    <div class="top-bar vertical medium-horizontal menu">
+
+    <div class="top-bar-left">
+      <h1 class="logo">abstract</h1>
+    </div>
+
+    <div class="top-bar-right">
+      <ul class="menu" style="background-color: transparent;">
+        <li class="top-btns">
+          <a href="#" class="pull">Menu</a>
+        </li>
+      </ul>
+    </div>
+
+    <div id="nav" dislay="block;">
+      <div id="nav-list" class="row small-centered">
+        <div class="small-4 columns small-centered">
+        <p>Contact Us</p>
+        <p>Jobs</p>
+        <a href="#" class="pull">Close</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+<!-- Jumbotron -->
+
+<div class="jumbotron">
+
+<div class="row small-centered">
+  <div class="small-10 small-centered columns">
+    <h1 class="call-out float-center">we are a cross disciplinary design studio.</h1>
+  </div>
+</div>
+
+</div>
+
+<div class="row expanded">
+    <div class=" scroll-btn">
+      <a href="#">
+        <span class="mouse">
+          <span>
+          </span>
+        </span>
+      </a>
+      <p></p>
+    </div>
+  </div>
+
+
+  <!-- End Jumbotron -->
+
+  <!-- Start Info Boxes -->
+
+  <div class="row section-two">
+    <div class="small-12 medium-6 columns">
+      <div>
+        <img src="img/info-box-1.png">
+      </div>
+    </div>
+    <div class="small-12 medium-6 columns">
+      <div class="row expanded section-two-content">
+        <div class="small-12 columns"><h1 class="info-box-title-right"><strong>building your<br> brand</strong></h1></div>
+        <div class="small-12 columns"><p class="info-box-text-right">Through refined approaches we focus on the balance of form and function to build a robust digital presence for your organization.  </p></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row section-three">
+    <div class="small-12 medium-6 columns image-stack-1">
+      <img src="img/info-box-2.png">
+    </div>
+    <div class="small-12 medium-6 columns">
+      <div class="row expanded section-two-content">
+        <div class="small-12 columns"><h1 class="info-box-title-left"><strong>designing with<br> passion</strong></h1></div>
+        <div class="small-12 columns float-right"><p class="info-box-text-left"> With attractive details and a bold, yet simplistic aesthetic, we work closely with you to deliver industry leading material that exemplifies your brands unique identity. Merging technology and art without compromise, we are able to follow current trends to deliver truly beautiful design solutions. </p></div>
+      </div>
+    </div>
+    <div class="small-12 medium-6 columns image-stack-2">
+      <img src="img/info-box-2.png">
+    </div>
+  </div>
+
+
+  <div class="row expanded section-four">
+    <div class="small-10 small-centered columns section-four-content">
+      <h4 class="section-four-text"><strong>we work with visionary companies and people to develop strategic marketing approaches that improve overall customer acquisition and retention.</strong> </h4>
+    </div>
+  </div>
+
+
+  <div class="row expanded">
+     <img data-interchange="[img/jumbotron-small.jpg, small], [img/jumbotron-medium.jpg, medium], [img/jumbotron.jpg, large]">
+  </div>
+
+  <div class="row expanded section-five">
+    <div style="text-align: center;" class="small-10 small-centered columns section-five-content">
+      <h1 class="section-five-header"><strong>got a project? say hello!</strong> </h1>
+      <p class="section-five-text">hello@abstract.digital<br>484.894.8886</p>
+    </div>
+  </div>
+
+
+
+
+      <script src="js/vendor/jquery.js"></script>
+      <script src="js/vendor/what-input.js"></script>
+      <script src="js/vendor/foundation.js"></script>
+      <script src="js/app.js"></script>
+    </body>
+  </html>
